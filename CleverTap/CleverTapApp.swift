637df4fa6d8f02ced1e870e033590e8a36044ccc
Huy Thach <tnyh02@gmail.com>
2026-04-18 10:55:28 +0700

    Initial Commit

---
+++ CleverTap/CleverTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleverTapApp.swift
+//  CleverTap
+//
+//  Created by huyoi on 18/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CleverTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
